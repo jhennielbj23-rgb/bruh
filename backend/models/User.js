c09e90c7483d9e731288a30707cd1a1a6e21e9e5
jhennielbj23-rgb <jhennielbj23@gmail.com>
2026-02-08 18:44:23 +0800

    Define User model with password hashing and methods

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,84 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
+const bcrypt = require('bcryptjs');
 
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  fullName: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  profilePhotoUrl: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  currency: {
+    type: DataTypes.STRING,
+    defaultValue: 'PHP'
+  },
+  timezone: {
+    type: DataTypes.STRING,
+    defaultValue: 'Asia/Manila'
+  },
+  emailVerified: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  resetPasswordToken: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  resetPasswordExpire: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+}, {
+  tableName: 'users',
+  timestamps: true,
+  hooks: {
+    beforeCreate: async (user) => {
+      if (user.password) {
+        const salt = await bcrypt.genSalt(12);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    },
+    beforeUpdate: async (user) => {
+      if (user.changed('password')) {
+        const salt = await bcrypt.genSalt(12);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    }
+  }
+});
+
+// Instance method to check password
+User.prototype.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+// Remove password from JSON response
+User.prototype.toJSON = function() {
+  const values = { ...this.get() };
+  delete values.password;
+  delete values.resetPasswordToken;
+  delete values.resetPasswordExpire;
+  return values;
+};
+
+module.exports = User;
