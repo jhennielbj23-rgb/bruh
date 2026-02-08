5d3cbac1a968807a3d385976107d044cd9a095f3
jhennielbj23-rgb <jhennielbj23@gmail.com>
2026-02-08 18:45:29 +0800

    Define Category model with attributes and defaults

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -1,1 +1,44 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/database');
 
+const Category = sequelize.define('Category', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  userId: {
+    type: DataTypes.UUID,
+    allowNull: true,
+    references: {
+      model: 'users',
+      key: 'id'
+    },
+    comment: 'Null for system default categories'
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  icon: {
+    type: DataTypes.STRING,
+    defaultValue: '📦'
+  },
+  color: {
+    type: DataTypes.STRING,
+    defaultValue: '#6366f1'
+  },
+  isCustom: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  isArchived: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  }
+}, {
+  tableName: 'categories',
+  timestamps: true
+});
+
+module.exports = Category;
